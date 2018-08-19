febd24e35b10e19f373c273865f7c3e13df05c52
Jacob Schellenberg <=>
2018-08-19 08:27:07 -0700

    var to strong type

--- Multiples_of_3_and_5/Program.cs
+++ Multiples_of_3_and_5/Program.cs
@@ -16,30 +16,29 @@ namespace MultiplesOf
 		
 		public static void Main ()
 		{
-			var sumOf01 = GetSumOfMultiplesOf(maxNaturalNumber, multipleOf: multipleOf01);
-			var sumOf02 = GetSumOfMultiplesOf(maxNaturalNumber, multipleOf: multipleOf02);
-			var sumOfMultiples = sumOf01 + sumOf02;
+			int sumOf01 = GetSumOfMultiplesOf(maxNaturalNumber, multipleOf: multipleOf01);
+			int sumOf02 = GetSumOfMultiplesOf(maxNaturalNumber, multipleOf: multipleOf02);
+			int sumOfMultiples = sumOf01 + sumOf02;
 
 			Console.WriteLine("Sum of {0}s and {1}s: {2}", multipleOf01, multipleOf02, sumOfMultiples);
 		}
 
 		private static int GetSumOfMultiplesOf(int maxNaturalNumber, int multipleOf)
 		{
-			var sum = 0;
+			int sum = 0;
 
-			for (var i = 0; i < maxNaturalNumber; i++) 
+			for (int i = 0; i < maxNaturalNumber; i++) 
 			{
-				var multiple = i * multipleOf;
+				int multiple = i * multipleOf;
 
 				if (multiple < maxNaturalNumber && multiple > 0) 
 				{
 					sum += multiple;
-					Console.WriteLine("Multiple of " + multipleOf + ": " + multiple);
+					Console.WriteLine("Multiple of {0}: {1}", multipleOf, multiple);
 				}
 			}
 			
-			Console.WriteLine ("Sum of multiples of " + multipleOf + ": " + sum);
-
+			Console.WriteLine("Sum of multiples of {0}: {1}", multipleOf, sum);
 			return sum;
 		}
 	}
